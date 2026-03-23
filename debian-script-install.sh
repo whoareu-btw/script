3133a24eef8fa50c7c79644c9076945b63b8a21c
Neko-chi <orangganteng87659@gmail.com>
2026-03-23 21:02:38 +0700

    Modify package installation to use --no-install-recommends

--- debian-script-install.sh
+++ debian-script-install.sh
@@ -24,9 +24,9 @@ run_task() {
 run_task "Updating repository and upgrading system" sudo apt update && sudo apt upgrade -y
 
 PACKAGE_SWAY="sway swaylock swaybg waybar rofi network-manager-applet pavucontrol nwg-look xwayland"
-run_task "Installing sway and related stuff" sudo apt install $PACKAGE_SWAY -y
+run_task "Installing sway and related stuff" sudo apt install -y --no-install-recommends $PACKAGE_SWAY
 
 PACKAGE_DEV="neovim podman distrobox"
-run_task "Installing dev tools" sudo apt install $PACKAGE_DEV -y
+run_task "Installing dev tools" sudo apt install -y --no-install-recommends $PACKAGE_DEV
 
 log_message "Proccess done, log saved in: $LOG_FILE"
